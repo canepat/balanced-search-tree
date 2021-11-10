c895ebe477bf6b208d48a51391febeeccec8036a
Tullio Canepa <tullio.canepa@gmail.com>
2021-11-10 10:13:42 +0100

    Move tree and node into separate modules

--- cairo-bptree/felt.go
+++ cairo-bptree/felt.go
@@ -1,3 +1,25 @@
 package cairo_bptree
 
+import "fmt"
+
 type Felt = uint64
+
+func pointerValue(pointer *Felt) string {
+	if pointer != nil {
+		return fmt.Sprintf("%d", *pointer)
+	} else {
+		return "<nil>"
+	}
+}
+
+func ptr2pte(pointers []*Felt) []Felt {
+	pointees := make([]Felt, 0)
+	for _, ptr := range pointers {
+		if ptr != nil {
+			pointees = append(pointees, *ptr)
+		} else {
+			break
+		}
+	}
+	return pointees
+}
